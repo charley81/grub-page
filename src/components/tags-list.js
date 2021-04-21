c081125c9ead64bc16e27a959970f1c9061c149c
ChrisKnowsDev <chrisharley81@gmail.com>
2021-04-21 19:43:17 -0400

    added tags to index page

--- src/components/tags-list.js
+++ src/components/tags-list.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import setupTags from '../utils/setup-tags'
+import { Link } from 'gatsby'
 
 const TagsList = ({ recipes }) => {
   const newTags = setupTags(recipes)
   return (
     <div>
-      <h4>Tags List</h4>
+      <h4>Recipes</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          const [text, value] = tag
+          return (
+            <Link key={index} to={`/${text}`}>
+              {text} ({value})
+            </Link>
+          )
+        })}
+      </div>
     </div>
   )
 }
